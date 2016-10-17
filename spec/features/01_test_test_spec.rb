7905c96d971c822afa8e978cfbaf3ccddadb1b23
Nathan W <nate01776@gmail.com>
2016-10-17 14:15:26 -0400

    added coveralls & schema file

---
+++ spec/features/01_test_test_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'Test should check spec files are set correctly' do
+
+  feature 'Test should always pass' do
+
+    scenario 'blank test should never fail' do
+    end
+  end
+end
