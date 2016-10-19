a66c86b44909035876eb4d8b1624fde8ed336988
Nathan W <nate01776@gmail.com>
2016-10-19 10:57:56 -0400

    registered user tests passing

--- spec/features/02_user_login_test_spec.rb
+++ spec/features/02_user_login_test_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe 'User sign in page' do
   feature 'should allow existing users to sign in' do
-    let!(:user_1) { create(:user) }
+    let!(:user_client) { create(:user) }
+    let!(:user_stylist) { create(:user, is_stylist: true) }
+    let!(:user_owner) { create(:user, is_owner: true) }
 
     scenario 'should see log in button on homepage' do
       visit '/'
@@ -20,13 +22,52 @@ describe 'User sign in page' do
     scenario 'should click log in and see an email and password field' do
       visit '/'
       click_link 'log in'
-      fill_in 'email', with: user_1.email
-      fill_in 'password', with: user_1.password
+      fill_in 'email', with: user_client.email
+      fill_in 'password', with: user_client.password
       click_button('Log in')
 
-      expect(page).to have_content(user_1.first_name)
-      expect(page).to have_content(user_1.last_name)
-      expect(page).to have_content(client)
+      expect(page).to have_content(user_client.username)
+      expect(page).to have_content(user_client.first_name)
+      expect(page).to have_content(user_client.last_name)
+    end
+
+    scenario 'client user should see client profile page' do
+      visit '/'
+      click_link 'log in'
+      fill_in 'email', with: user_client.email
+      fill_in 'password', with: user_client.password
+      click_button('Log in')
+
+      expect(page).to have_content(user_client.username)
+      expect(page).to have_content(user_client.first_name)
+      expect(page).to have_content(user_client.last_name)
+      expect(page).to have_content('client')
+    end
+
+    scenario 'stylist user should see stylist profile page' do
+      visit '/'
+      click_link 'log in'
+      fill_in 'email', with: user_stylist.email
+      fill_in 'password', with: user_stylist.password
+      click_button('Log in')
+
+      expect(page).to have_content(user_stylist.username)
+      expect(page).to have_content(user_stylist.first_name)
+      expect(page).to have_content(user_stylist.last_name)
+      expect(page).to have_content('stylist')
+    end
+
+    scenario 'owner user should see owner profile page' do
+      visit '/'
+      click_link 'log in'
+      fill_in 'email', with: user_owner.email
+      fill_in 'password', with: user_owner.password
+      click_button('Log in')
+
+      expect(page).to have_content(user_owner.username)
+      expect(page).to have_content(user_owner.first_name)
+      expect(page).to have_content(user_owner.last_name)
+      expect(page).to have_content('owner')
     end
   end
 end
