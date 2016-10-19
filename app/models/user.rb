8b5a5288e6ffbe0f0746bc76a2d87ff8400dde9e
Nathan W <nate01776@gmail.com>
2016-10-19 11:42:44 -0400

    updated user model to meet hound

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   validates :first_name, presence: true
-   validates :last_name, presence: true
-   validates :username, presence: true, uniqueness: true
-   validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, uniqueness: true
-   validates :password, presence: true, length: { minimum: 6 }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
 
-   def status
-     if self.is_owner
-       return "owner"
-     elsif self.is_stylist
-       return "stylist"
-     else
-       return "client"
-     end
-   end
+    def status
+      if is_owner
+        return "owner"
+      elsif is_stylist
+        return "stylist"
+      else
+        return "client"
+      end
+    end
 end
