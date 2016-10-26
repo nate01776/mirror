a01ae5c1fffc8abb209a2caefab344eb882e1ef9
Nathan W <nate01776@gmail.com>
2016-10-25 22:12:02 -0400

    removed border

--- spec/features/03_owner_tests/08_owner_can_delete_products_and_services_spec.rb
+++ spec/features/03_owner_tests/08_owner_can_delete_products_and_services_spec.rb
@@ -9,17 +9,17 @@ describe 'Owner can delete salon service and products' do
     let!(:salon_2) { create(:salon, owner_id: user_owner_2.id) }
     let!(:service) { create(:service, salon_id: salon.id) }
     let!(:product) { create(:product, salon_id: salon.id) }
-
-    before :each do
+    let!(:owner_sign_in) {
       visit '/'
       click_link 'log in'
       fill_in 'email', with: user_owner.email
       fill_in 'password', with: user_owner.password
       click_button('log in')
-    end
+    }
 
     scenario 'should see delete links below products' do
       salon.products << product
+      owner_sign_in
       visit salon_path(salon.id)
 
       expect(page).to have_content(product.name)
@@ -28,6 +28,7 @@ describe 'Owner can delete salon service and products' do
 
     scenario 'should remove product when link is clicked' do
       salon.products << product
+      owner_sign_in
       visit salon_path(salon.id)
       click_link 'Delete this product'
 
@@ -36,6 +37,7 @@ describe 'Owner can delete salon service and products' do
 
     scenario 'should see delete links below services' do
       salon.services << service
+      owner_sign_in
       visit salon_path(salon.id)
 
       expect(page).to have_content(service.name)
@@ -44,6 +46,7 @@ describe 'Owner can delete salon service and products' do
 
     scenario 'should remove service when link is clicked' do
       salon.services << service
+      owner_sign_in
       visit salon_path(salon.id)
       click_link 'Delete this service'
 
