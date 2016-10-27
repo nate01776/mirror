53b4c6ad4c243560098f5192be93519100859046
Nathan W <nate01776@gmail.com>
2016-10-27 13:31:57 -0400

    stylists updated to show clients and upcoming appts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,22 @@ class UsersController < ApplicationController
     end
     if @status == 'stylist'
       @stylist_services = Stylistservice.where("user_id = #{@user.id}")
+      @clients = []
       @client_appts = []
       Clientservice.all.each do |appt|
         if appt.stylist.id == @user.id
+          @client_appts << appt
+          if !@clients.include?(User.find(appt.user_id))
+            @clients << User.find(appt.user_id)
+          end
+        end
+      end
+      @client_appts = @client_appts.sort_by{ |appt| appt.datetime }
+    end
+    if @status == 'client'
+      @client_appts = []
+      Clientservice.all.each do |appt|
+        if appt.user_id == @user.id
           @client_appts << appt
         end
       end
