526804197888ad50fe49169467710ca961ee506e
Nathan W <nate01776@gmail.com>
2016-10-27 21:43:57 -0400

    migration ready to run

--- db/migrate/20161028012916_change_clientservices_table.rb
+++ db/migrate/20161028012916_change_clientservices_table.rb
@@ -1,4 +1,12 @@
 class ChangeClientservicesTable < ActiveRecord::Migration[5.0]
-  def change
+  def up
+    rename_column :clientservices, :stylistservice_id, :stylist_id, null: false
+    add_column :clientservices, :service_id, :integer, null: false
+    add_column :clientservices, :salon_id, :integer, null: false
   end
+
+  def down
+    rename_column :clientservices, :stylist_id, :stylistservice_id
+    remove_column :clientservices, :service_id, :integer, null: false
+    remove_column :clientservices, :salon_id, :integer, null: false  end
 end
